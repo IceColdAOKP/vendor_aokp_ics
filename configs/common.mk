04f7adf183cf87e0c8dbbca8d603b95386d82e74
LorD ClockaN <davor@losinj.com>
2012-07-09 22:21:48 +0200

    don't build Apollo

--- configs/common.mk
+++ configs/common.mk
@@ -15,8 +15,7 @@ PRODUCT_PACKAGES += \
     openvpn \
     SwagPapers \
     AOKPtips \
-    FileManager \
-    Apollo
+    FileManager
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
